22f1f8f8afd0dcbc53f04e7615d63b6cea85154b
Ganesh Utla <ganeshutla888@gmail.com>
2024-03-23 01:28:41 +0530

    ..

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,9 @@
-import Banner from './components/Banner/Banner';
-import Companies from './components/Companies/Companies';
-import Tabs from './components/Courses/Courses';
-import Mentor from './components/Mentor/Mentor';
-import Students from './components/Students/Students';
-import Newsletter from './components/Newsletter/Newsletter';
+import Banner from '../components/Banner/Banner';
+import Companies from '../components/Companies/Companies';
+import Tabs from '../components/Courses/Courses';
+import Mentor from '../components/Mentor/Mentor';
+import Students from '../components/Students/Students';
+import Newsletter from '../components/Newsletter/Newsletter';
 
 export default function Home() {
   return (
