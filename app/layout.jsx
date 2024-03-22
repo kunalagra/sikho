c22cbce414b47e024f77e855825bdf86ac98a04a
Ganesh Utla <ganeshutla888@gmail.com>
2024-03-23 01:44:06 +0530

    ..

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,6 @@
 import './globals.css';
-import Navbar from './components/Navbar/index';
-import Footer from './components/Footer/Footer';
+import Navbar from '../components/Navbar/index';
+import Footer from '../components/Footer/Footer';
 
 
 export const metadata = {
