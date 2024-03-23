006293c2766f1992a1024877429186dd74d0d5cf
Vinay <vinaysinghvsk8803@gmail.com>
2024-03-23 15:15:08 +0530

    made a pricing section and catgories

---
+++ app/api/Categories/categoryInfo.js
@@ -0,0 +1,28 @@
+export const categoryInfo = [
+  {
+    "name": "Web Development",
+    "count": 2,
+    "thumbnail": "/assets/courses/14_websitedev.jpg"
+  },
+  {
+    "name": "Machine Learning & Data Science ",
+    "count": 1,
+    "thumbnail":  "/assets/courses/17_ailogo.jpg"
+  },
+  {
+    "name": " Programming Languages ",
+    "count": 5,
+    "thumbnail": "/assets/courses/15_javalogo.png"
+  },
+  {
+    "name": "Data Structures and Algorithm ",
+    "count": 26,
+    "thumbnail": "/assets/courses/4 _dsa_dev.png"
+  },
+  {
+    "name": "Database and Cloud Courses ",
+    "count": 11,
+    "thumbnail": "/assets/courses/12_complete_sql.jpg"
+  }
+]
+
--- app/page.jsx
+++ app/page.jsx
@@ -4,16 +4,23 @@ import Tabs from '../components/student/Courses/Courses';
 import Mentor from '../components/student/Mentor/Mentor';
 import Students from '../components/student/Students/Students';
 import Newsletter from '../components/student/Newsletter/Newsletter';
+import Category from '../components/student/Categories/Category';
+import Instructor from '../components/Instructor/Instructor';
+import Plans from '../components/Plans/Plans';
+
 
 export default function Home() {
   return (
     <main>
       <Banner />
       <Companies />
+      <Category/>
       <Tabs />
       <Mentor />
+      <Instructor/>
       <Students />
       <Newsletter />
+ 
     </main>
   )
 }
