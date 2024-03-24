f3ce44d5eed866c58af6456b9d2186d958aa4b7d
Deexith Madas <deexithmadas772@gmail.com>
2024-03-24 08:41:22 +0530

    done

--- app/success/page.jsx
+++ app/success/page.jsx
@@ -1,21 +1,24 @@
 "use client";
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
+import ScheduleMeeting from '@/components/Course/ScheduleMeeting';
 
 const SuccessPage = () => {
     const navigate = useRouter();
-    setTimeout(() => {
-        navigate.push('/');
-    }, 5000);
+    // setTimeout(() => {
+    //     navigate.push('/');
+    // }, 5000);
 
     return (
-        <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
-        <div className="bg-white p-8 rounded-lg shadow-lg">
-          <h1 className="text-3xl font-semibold text-green-600 mb-4">Payment Successful!</h1>
-          <p className="text-lg text-gray-800">Thank you for your purchase. Your order has been successfully processed.</p>
-            <p className="text-lg text-gray-800 mt-4">You will be redirected to the home page in 5 seconds.</p>
-        </div>
-      </div>
+    //     <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
+    //     <div className="bg-white p-8 rounded-lg shadow-lg">
+    //       <h1 className="text-3xl font-semibold text-green-600 mb-4">Payment Successful!</h1>
+    //       <p className="text-lg text-gray-800">Thank you for your purchase. Your order has been successfully processed.</p>
+    //         <p className="text-lg text-gray-800 mt-4">You will be redirected to the home page in 5 seconds.</p>
+    //     </div>
+    //   </div>
+
+    <ScheduleMeeting />
         
     );
     }
