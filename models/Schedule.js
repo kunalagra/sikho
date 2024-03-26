ab61e2275fec4e865499a07d1ad2725852a3b396
Ganesh Utla <ganeshutla888@gmail.com>
2024-03-26 17:25:52 +0530

    fixed True bug

--- models/Schedule.js
+++ models/Schedule.js
@@ -3,8 +3,8 @@ import {model, models, Schema} from "mongoose";
 const ScheduleSchema = new Schema({
   lessons: { type: Schema.Types.ObjectId, ref: 'LessonPlan'},
 //   reviews: [{ type: Schema.Types.ObjectId, ref: 'PeerVideoReview', default: [] }],
-  start: { type: Date, required: True},
-  end: { type: Date, required: True},
+  start: { type: Date, required: true},
+  end: { type: Date, required: true},
   meetid: {type: String},
   title: {type: String}
 }, {timestamps: true});
